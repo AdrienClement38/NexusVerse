edf7185da8517818574a5cebcb46a07766e44a2f
Kiliroy <borges.mathieu@gmail.com>
2023-05-23 17:48:37 +0200

    Fixed TeamController Todo : LeagueController, view team view PlayerController, view

--- src/Controller/TeamController.php
+++ src/Controller/TeamController.php
@@ -3,21 +3,22 @@
 namespace App\Controller;
 
 use App\Repository\PlayerRepository;
+use App\Repository\TeamRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TeamController extends AbstractController
 {
-    #[Route('/team', name: 'app_team')]
-    public function index(PlayerRepository $playerRepository): Response
+    #[Route('/teams', name: 'app_teams')]
+    public function index(TeamRepository $teamRepository): Response
     {
 
-        $playerRepositoryArray = $playerRepository->findAll();
+        $teams = $teamRepository->findAll();
 
         return $this->render('team/index.html.twig', [
             'controller_name' => 'TeamController',
-            'players' => $playerRepositoryArray,
+            'teams' => $teams,
         ]);
     }
 }
