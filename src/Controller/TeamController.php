60925a50ef5613efb922386ec5442fd6c6fe1822
Adrien Clement <adrienclement38@gmail.com>
2023-05-24 14:53:46 +0200

    work in progress

--- src/Controller/TeamController.php
+++ src/Controller/TeamController.php
@@ -2,23 +2,33 @@
 
 namespace App\Controller;
 
-use App\Repository\PlayerRepository;
+use App\Entity\Team;
 use App\Repository\TeamRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/team')]
 class TeamController extends AbstractController
 {
-    #[Route('/teams', name: 'app_teams')]
+    #[Route('/', name: 'app_team_index', methods: ['GET'])]
     public function index(TeamRepository $teamRepository): Response
     {
-
-        $teams = $teamRepository->findAll();
-
         return $this->render('team/index.html.twig', [
-            'controller_name' => 'TeamController',
-            'teams' => $teams,
+            'teams' => $teamRepository->findAll(),
         ]);
     }
+
+    #[Route('/{id}', name: 'app_team_show', methods: ['GET'])]
+    public function show(Team $team): Response
+    {
+
+        $encounters = $team->getEncounters();
+    
+        return $this->render('team/show.html.twig', [
+            'team' => $team,
+            'encounters' => $encounters
+        ]);
+    }
+
 }
