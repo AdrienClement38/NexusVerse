7b56f521e3a79e31d48c168353053f5a2bdc2d5e
Kiliroy <borges.mathieu@gmail.com>
2023-05-24 09:34:24 +0200

    Fixed logout, register

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function index(LeagueRepository $leagueRepository, ChampionRepository $championRepository): Response
     {
 
