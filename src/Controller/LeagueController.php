00df0fae9abe8c3b900a9e3237a70b5b71d864b6
Kiliroy <borges.mathieu@gmail.com>
2023-05-25 15:28:21 +0200

    Updated views and LeagueController to display scores

--- src/Controller/LeagueController.php
+++ src/Controller/LeagueController.php
@@ -6,6 +6,7 @@ use App\Entity\League;
 use App\Entity\Team;
 use App\Repository\LeagueRepository;
 use App\Repository\TeamRepository;
+use App\Services\RankingService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,40 +22,50 @@ class LeagueController extends AbstractController
         ]);
     }
 
+    // #[Route('/{id}', name: 'app_league_show', methods: ['GET'])]
+    // public function show(League $league, LeagueRepository $leagueRepository, TeamRepository $teamRepository): Response
+    // {
+    //     //obtenir les equipes de la ligue
+    //     $encounters = $league->getTournament()->getEncounters();
+
+    //     // Obtenez les équipes, la ligue, etc. à partir des rencontres
+    //     $encountersArray = $encounters->map(function ($encounter) {
+    //         $team1 = $encounter->getTeams()->first();
+    //         $team2 = $encounter->getTeams()->last();
+    //         $score1 = $encounter->getScores()->first();
+    //         $score2 = $encounter->getScores()->last();
+    //         $date = $encounter->getDate();
+    //         $tournament = $encounter->getTournament();
+
+
+    //         return [
+    //             'team1' => $team1,
+    //             'team2' => $team2,
+    //             'score1' => $score1,
+    //             'score2' => $score2,
+    //             'date' => $date,
+    //             'tournament' => $tournament,
+    //         ];
+    //     });
+
+
+
+    //     return $this->render('league/show.html.twig', [
+
+    //         'league' => $league,
+    //         'encountersArray' => $encountersArray,   
+    //         'encounters' => $encounters,         
+    //     ]);
+    // }
     #[Route('/{id}', name: 'app_league_show', methods: ['GET'])]
-    public function show(League $league, LeagueRepository $leagueRepository, TeamRepository $teamRepository): Response
+    public function show(League $league, RankingService $rankingService): Response
     {
-        //obtenir les equipes de la ligue
-        $encounters = $league->getTournament()->getEncounters();
-    
-        // Obtenez les équipes, la ligue, etc. à partir des rencontres
-        $encountersArray = $encounters->map(function ($encounter) {
-            $team1 = $encounter->getTeams()->first();
-            $team2 = $encounter->getTeams()->last();
-            $score1 = $encounter->getScores()->first();
-            $score2 = $encounter->getScores()->last();
-            $date = $encounter->getDate();
-            $tournament = $encounter->getTournament();
-            
-    
-            return [
-                'team1' => $team1,
-                'team2' => $team2,
-                'score1' => $score1,
-                'score2' => $score2,
-                'date' => $date,
-                'tournament' => $tournament,
-            ];
-        });
-        
-        
-    
+        $rankings = $rankingService->getLeagueResult($league);
+
         return $this->render('league/show.html.twig', [
 
             'league' => $league,
-            'encountersArray' => $encountersArray,   
-            'encounters' => $encounters,         
+            'rankings' => $rankings,
         ]);
     }
 }
-
